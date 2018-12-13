ea930427795b65a84317be9bee79f7b8760b062c
ryuash <ryuash123@gmail.com>
2018-12-12 19:11:06 -0500

    got express and react

--- server/api/users.js
+++ server/api/users.js
@@ -1,4 +1,4 @@
-const router = require('express').Router;
+const router = require('express').Router();
 
 //this is for /api/users
 router.get('/', async (req, res, next) => {
--- server/index.js
+++ server/index.js
@@ -4,22 +4,24 @@ const morgan = require('morgan');
 const path = require('path');
 
 //middleware
-// app.use(morgan('dev'));
-// app.use(express.static(path.join(__dirname, '..', '/public')));
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use('/api', require('./api'));
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '..', '/public')));
+
+app.use('/api', require('./api')); // include our routes!
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'public/index.html'));
 });
 
-//server error 500
-// app.use((err, req, res, next) => {
-//     console.error(err);
-//     console.error(err.stack);
-//     res.status(err.status || 500).send(err.message || 'Internal server error.');
-// });
+// server error 500
+app.use((err, req, res, next) => {
+    console.error(err);
+    console.error(err.stack);
+    res.status(err.status || 500).send(err.message || 'Internal server error.');
+});
 
 //start server
 const port = process.env.PORT || 3000;
@@ -35,5 +37,5 @@ const test = () => {
     }
 };
 
-// test();
+test();
 
