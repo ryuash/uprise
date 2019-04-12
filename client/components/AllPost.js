3261c4c74d4049cfea9448d6686c2d1a6459b60c
ryuash <ryuash123@gmail.com>
2019-04-12 12:20:57 -0400

    allpost loads correctly working on single post

--- client/components/AllPost.js
+++ client/components/AllPost.js
@@ -1,18 +1,75 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
+const styles = theme => ({
+    root: {
+        width: '50%',
+        maxHeight: '50vh',
+        marginTop: theme.spacing.unit * 3,
+        overflowY: 'auto',
+    },
+    tableRow: {
+        '&:hover': {
+            background: 'pink',
+            cursor: 'pointer'
+        }
+    },
+    table: {
+        // minWidth: 700,
+    },
+});
 
-const AllPost = props => {
-    console.log(props.allPost, 'all post');
-    return (
-        <h1>hit all post</h1>
-    );
-};
+class AllPost extends React.Component {
+    constructor() {
+        super();
+        this.handleClick = this.handleClick.bind(this);
+    }
+    handleClick(e) {
+        this.props.history.push(`/post/${e}`);
+    }
+    render() {
+        const { classes } = this.props;
+        return (
+            <div id="allPostContainer">
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="left">Id</TableCell>
+                                <TableCell align="left">Title</TableCell>
+                                <TableCell align="left">User</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {this.props.allPost.map(x => (
+                                <TableRow key={x.title} className={classes.tableRow} onClick={() => this.handleClick(x.id)}>
+                                    <TableCell align="left">
+                                        {x.id}
+                                    </TableCell>
+                                    <TableCell align="left">{x.title}</TableCell>
+                                    {this.props.allUser.length && <TableCell align="left">{this.props.allUser[x.userId - 1].username}</TableCell>}
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </div>
+        );
+    }
+}
+
 
 const mapState = state => {
     return {
         allPost: state.Post.allPost,
-        allUser: state.Post.allUser
+        allUser: state.User.allUser
     };
 };
-export default connect(mapState)(AllPost);
+export default connect(mapState)(withStyles(styles)(AllPost));
